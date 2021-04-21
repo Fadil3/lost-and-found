23bda0bf3bc2c00ac08f99f9e697cfbbb6d2f2ae
LAPTOP-SAR00LI0\USER <muh.ilham.malik@gmail.com>
2021-04-21 15:07:27 +0700

    Update untuk sosmed dan roles. Untuk roles jika dia user auto 1 keisinya

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -22,17 +22,22 @@ class Register extends Controller
             'name'          => 'required',
             'email'         => 'required|valid_email|is_unique[user.user_email]',
             'password'      => 'required',
-            'no_telepon'  => 'required'
+            'no_telepon'    => 'required',
+            'instagram'     => 'required',
+            'facebook'      => 'required'
         ];
          
         if($this->validate($rules)){
             $model = new UserModel();
             $data = [
-                'user_name'     => $this->request->getVar('name'),
-                'user_email'    => $this->request->getVar('email'),
-                'user_password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-                'user_no_telepon' => $this->request->getVar('no_telepon')
+                'user_name'       => $this->request->getVar('name'),
+                'user_email'      => $this->request->getVar('email'),
+                'user_password'   => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+                'user_no_telepon' => $this->request->getVar('no_telepon'),
+                'user_instagram'  => $this->request->getVar('instagram'),
+                'user_facebook'   => $this->request->getVar('facebook')
             ];
+            
             $model->save($data);
             return redirect()->to('/pages/login');
         }else{
