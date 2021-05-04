08e3878c01a0ca2e6128547f64bc96abc1c6c37e
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-05-04 10:00:31 +0700

    add facebook & twitter share button

--- app/Views/Pages/detail_lap_kehilangan.php
+++ app/Views/Pages/detail_lap_kehilangan.php
@@ -21,9 +21,10 @@
                     <p>Ciri-ciri Barang : <?= $barang['deskripsi_barang']; ?></p>
                     <p>Pemilik Barang : xxxxxxx</p>
                     <p>Hubungi Pencari melalui</p>
-                    <a href="#" class="btn btn-success mt-2 mr-5">Whatsapp</a>
-                    <a href="#" class="btn btn-danger mt-2 mr-5">E-Mail</a>
-                    <a href="#" class="btn btn-primary mt-2">Facebook</a>
+                    <a href="#" class="btn btn-success mt-2 mr-5"><i class="fab fa-whatsapp mr-1"></i>Whatsapp</a>
+                    <a href="#" class="btn btn-danger mt-2 mr-5"><i class="far fa-envelope mr-1"></i>E-Mail</a>
+                    <a href="#" class="btn btn-primary mt-2"><i class="fab fa-facebook-f mr-1"></i>Facebook</a>
+                    <a href="#" class="btn btn-secondary mt-2"><i class="fab fa-instagram mr-1"></i>Instagram</a>
                 </div>
             </div>
         </section>
@@ -39,6 +40,27 @@
             <button class="btn btn-light mt-2 mb-5 p-2" style="background-color: #8F00FF;" type="button">
                 <span class="text-white">Saya telah menemukan barang Ini!</span>
             </button>
+        </div>
+    </div>
+
+    <?php  
+    $twitter ="[DICARI] ".$barang['nama_barang']."\nLokasi hilang : ".$barang['lokasi_barang']."\nCiri-ciri : ".$barang['deskripsi_barang'];
+    $url = current_url();
+    $tes = "http://mrayhanfadil.cehiji.com/";
+    ?>
+    <div class="container">
+        <div class="col-md-8 mx-auto text-center">
+            <p class="font-weight-bold">Share Laporan ini ke sosial media</p>
+            <div class="row d-flex justify-content-around">
+                <a href="https://twitter.com/intent/tweet/?text=<?= urlencode($twitter); ?>&amp;url=<?= urlencode($url); ?>"
+                    target="_blank" rel="noopener" class="btn btn-info mt-2 "><i
+                        class="fab fa-twitter mr-1"></i>Twitter</a>
+                <button onclick="copyToClipboard()" class="btn btn-secondary mt-2"><i class="far fa-copy mr-1"></i>Copy
+                    Url
+                    Laporan ini</button>
+                <a href="https://facebook.com/sharer/sharer.php?u=<?= urlencode($tes); ?>" target="_blank"
+                    rel="noopener" class="btn btn-primary mt-2"><i class="fab fa-facebook-f mr-1"></i>Facebook</a>
+            </div>
         </div>
     </div>
 </div>
