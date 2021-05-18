b6e5722b165a54b2a166dd30de93742ba679c705
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-05-18 21:37:03 +0700

    redirect to succes page after klaim laporan

--- app/Views/Pages/success.php
+++ app/Views/Pages/success.php
@@ -2,13 +2,14 @@
 
 <?= $this->section('content'); ?>
 <div class="container mb-5">
-    <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-        <img src="images/success.png" class="mt-3 mb-2" alt=" fe-template"/>
+    <div class="d-grid gap-2 d-flex justify-content-center">
+        <img src="images/success.png" style="max-width: 300px;" class=" mt-3 mb-2" alt=" fe-template" />
     </div>
-    <h3 class="ml-3 mb-4 sub-title text-center">
+    <h3 class="ml-3 mb-4  mx-auto d-flex justify-content-center sub-title text-center">
         Klaim Barang Telah Berhasil!
     </h3>
-    <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-        <button class="btn btn-primary ml-3 mb-5" type="button">Kembali ke Home</button>
+    <div class="d-grid gap-2 mx-auto d-flex justify-content-center">
+        <a class="btn btn-primary ml-3 mb-5" href="/">Kembali ke Home</a>
     </div>
 </div>
+<?= $this->endSection(); ?>
